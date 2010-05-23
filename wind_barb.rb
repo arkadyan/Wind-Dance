815b6099b17d03f06f3cd31f60907a7eaa01cc1e
arkadyan <matthewshanley@littlesecretsrecords.com>
2010-05-23 15:13:30 -0400

    Extracted out display size and color controls into class constants.

--- wind_barb.rb
+++ wind_barb.rb
@@ -6,11 +6,21 @@ class WindBarb
 	include Processing::Proxy
 	
 	attr_accessor :speed, :direction, :pos, :previous_barb
+	
+	MAIN_LINE_LENGTH = 30
+	DOT_WIDTH = 3
+	CIRCLE_WIDTH = 20
+
+	# Arrow head height and (center-to-edge) width
+	ARROW_LINE_LENGTH = 30
+	ARROW_HEAD_HEIGHT = 8
+	ARROW_HEAD_WIDTH = 4
+	ARROW_STROKE_COLOR = 50
+	ARROW_STROKE_WEIGHT = 1
+	
 
 	def initialize(speed, direction, previous_barb)
 		@pos = Point.new(width/2, height/2)
-		@dot_width = 3
-		@circle_width = 20
 		
 		@speed = speed
 		@direction = direction
@@ -57,7 +67,7 @@ class WindBarb
 	end
   
 	def render
-		if speed == 0
+		if @speed == 0
 			render_calm
 		else
 			render_step
@@ -68,53 +78,46 @@ class WindBarb
 	private
 	
 	def render_calm
-		# Arrow head height and (center-to-edge) width
-		arrow_line_length = 30
-		arrow_head_height = 8
-		arrow_head_width = 4
-		arrow_stroke_color = 50
-		arrow_stroke_weight = 1
-		
-		stroke arrow_stroke_color
-		stroke_weight arrow_stroke_weight
+		stroke ARROW_STROKE_COLOR
+		stroke_weight ARROW_STROKE_WEIGHT
 				
 		# Draw center dot
 		fill 0
-		ellipse @pos.x, @pos.y, @dot_width, @dot_width
+		ellipse @pos.x, @pos.y, DOT_WIDTH, DOT_WIDTH
 		
 		# Draw outer circle
 		no_fill
-		ellipse @pos.x, @pos.y, @circle_width, @circle_width
+		ellipse @pos.x, @pos.y, CIRCLE_WIDTH, CIRCLE_WIDTH
 		
 		
 		push_matrix
 		translate @pos.x, @pos.y
 		rotate 3*Math::PI/2 + direction_in_radians
 		
-		stroke arrow_stroke_color
+		stroke ARROW_STROKE_COLOR
 		
 		# Draw the arrow line
-		line 0, 0, 0, arrow_line_length
+		line 0, 0, 0, ARROW_LINE_LENGTH
 		
 		# Draw the arrow head
-		fill arrow_stroke_color
-		triangle 0, arrow_line_length, -arrow_head_width, arrow_line_length-arrow_head_height, arrow_head_width, arrow_line_length-arrow_head_height
+		fill ARROW_STROKE_COLOR
+		triangle 0, ARROW_LINE_LENGTH, -ARROW_HEAD_WIDTH, ARROW_LINE_LENGTH-ARROW_HEAD_HEIGHT, ARROW_HEAD_WIDTH, ARROW_LINE_LENGTH-ARROW_HEAD_HEIGHT
 		no_fill
-		# line 0, 0, arrow_head_width, -arrow_head_height
-		# line 0, 0, -arrow_head_width, -arrow_head_height
+		# line 0, 0, ARROW_HEAD_WIDTH, -ARROW_HEAD_HEIGHT
+		# line 0, 0, -ARROW_HEAD_WIDTH, -ARROW_HEAD_HEIGHT
 		pop_matrix
 	end
 	
 	def render_step
-		main_line_length = 30
+		
 
-		from_point = Point.new(@pos.x - (main_line_length/2)*Math.cos(direction_in_radians), pos.y - (main_line_length/2)*Math.sin(direction_in_radians))
-		# to_point = Point.new(@pos.x + (main_line_length/2)*Math.cos(direction_in_radians), pos.y + (main_line_length/2)*Math.sin(direction_in_radians))
-		to_point = Point.new(from_point.x+main_line_length*Math.cos(direction_in_radians), from_point.y+main_line_length*Math.sin(direction_in_radians))
+		from_point = Point.new(@pos.x - (MAIN_LINE_LENGTH/2)*Math.cos(direction_in_radians), pos.y - (MAIN_LINE_LENGTH/2)*Math.sin(direction_in_radians))
+		# to_point = Point.new(@pos.x + (MAIN_LINE_LENGTH/2)*Math.cos(direction_in_radians), pos.y + (MAIN_LINE_LENGTH/2)*Math.sin(direction_in_radians))
+		to_point = Point.new(from_point.x+MAIN_LINE_LENGTH*Math.cos(direction_in_radians), from_point.y+MAIN_LINE_LENGTH*Math.sin(direction_in_radians))
 		
 		line from_point.x, from_point.y, to_point.x, to_point.y
 
-		render_barbs(from_point, main_line_length, @speed)
+		render_barbs(from_point, MAIN_LINE_LENGTH, @speed)
 	end
 	
 	def render_barbs(from_point, length, speed)
