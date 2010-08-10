a4914116c5ec572a86caf5c1ec1e6ff782579c1e
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2010-08-09 20:45:38 -0400

    Went through the first dozen days adding offsets where appropriate.

--- runner.rb
+++ runner.rb
@@ -7,14 +7,24 @@
 	when '07.08.2009'
 		starting_offset_x = 6500
 		starting_offset_y = 6500
+	when '09.08.2009'
+		starting_offset_x = 1000
+		starting_offset_y = 1000
+	when '10.08.2009'
+		starting_offset_x = 5000
+		starting_offset_y = 5000
+	when '11.08.2009'
+		starting_offset_x = 5000
+		starting_offset_y = 5000
 	else
 		starting_offset_x = 0
 		starting_offset_y = 0
 	end
 	
-	# `rp5 run dance_diagram.rb DanceDiagram 700 700 #{date} data/spws-data-flux-809-data_only.csv #{date}`
-	# `rp5 run dance_diagram.rb DanceDiagram 1600 1600 #{date} data/spws-data-flux-809-data_only.csv #{date}`
-	# `rp5 run dance_diagram.rb DanceDiagram 2400 2400 #{date} data/spws-data-flux-809-data_only.csv #{date}`
-	# `rp5 run dance_diagram.rb DanceDiagram 3600 3600 #{date} data/spws-data-flux-809-data_only.csv #{date}`
-	`rp5 run dance_diagram.rb DanceDiagram 7200 7200 #{date} data/spws-data-flux-809-data_only.csv #{date} #{starting_offset_x} #{starting_offset_y}`
+	# size = 700
+	# size = 1600
+	# size =  2400
+	# size =  3600
+	size =  7200
+	`rp5 run dance_diagram.rb DanceDiagram #{size} #{size} #{date} data/spws-data-flux-809-data_only.csv #{date} #{starting_offset_x} #{starting_offset_y}`
 end
