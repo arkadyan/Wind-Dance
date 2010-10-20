e8d44d5773cc507bd94951abb6e5945f9736c78b
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2010-10-20 19:43:23 -0400

    Adjusted starting offsets to work with the new data and spacing.

--- thoreau_runner.rb
+++ thoreau_runner.rb
@@ -2,8 +2,8 @@
 	date = "#{"%02d" % day}.10.1844"
 	puts "date = #{date}"
 	
-	starting_offset_x = 0
-	starting_offset_y = 0
+	starting_offset_x = 2200
+	starting_offset_y = 3200
 	
 	size = 4000
 	puts `rp5 run dance_diagram.rb DanceDiagram #{size} #{size} #{date} data/thoreau_data.csv #{date} #{starting_offset_x} #{starting_offset_y} 0 23`
