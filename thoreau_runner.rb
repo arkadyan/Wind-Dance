ac671b1d5eb4b3b0f843d85277ede5cb93038101
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2010-10-23 13:45:14 -0400

    Added the orginal Thoreau date to the new data file and update the runner to use this new data file and run through all 5 dates.

--- thoreau_runner.rb
+++ thoreau_runner.rb
@@ -1,10 +1,9 @@
-(01..01).each do |day|
-	date = "#{"%02d" % day}.10.1844"
+['01.10.1844','25.03.1846','08.04.1847','28.03.1851','23.03.1853'].each do |date|
 	puts "date = #{date}"
 	
 	starting_offset_x = 2200
 	starting_offset_y = 3200
 	
 	size = 4000
-	puts `rp5 run dance_diagram.rb DanceDiagram #{size} #{size} #{date} data/thoreau_data.csv #{date} #{starting_offset_x} #{starting_offset_y} 0 23`
+	puts `rp5 run dance_diagram.rb DanceDiagram #{size} #{size} #{date} data/thoreau_1840s_1850s.csv #{date} #{starting_offset_x} #{starting_offset_y} 0 23`
 end
