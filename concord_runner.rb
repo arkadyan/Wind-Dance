552d485e0d7b1846f26418945cb64adafeed444d
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2010-10-24 22:18:23 -0400

    Updated the runner to use the new data file, run through all 5 days, and added offsets to make them fit properly.

--- concord_runner.rb
+++ concord_runner.rb
@@ -1,10 +1,24 @@
-(01..01).each do |day|
-	date = "#{"%02d" % day}.02.2010"
+['21.02.2010','12.04.2001','23.02.2002','05.04.2005','13.03.2006'].each do |date|
 	puts "date = #{date}"
 	
-	starting_offset_x = 3800
-	starting_offset_y = 0
+	case date
+	when '21.02.2010'
+		starting_offset_x = 3300
+		starting_offset_y = 0
+	when '12.04.2001'
+		starting_offset_x = 500
+		starting_offset_y = 1800
+	when '23.02.2002'
+		starting_offset_x = 3000
+		starting_offset_y = 2800
+	when '05.04.2005'
+		starting_offset_x = 3700
+		starting_offset_y = 2700
+	else
+		starting_offset_x = 0
+		starting_offset_y = 0
+	end
 	
 	size = 4000
-	puts `rp5 run dance_diagram.rb DanceDiagram #{size} #{size} #{date} data/concord_feb_2010_wind.csv #{date} #{starting_offset_x} #{starting_offset_y} 0 23`
+	puts `rp5 run dance_diagram.rb DanceDiagram #{size} #{size} #{date} data/concord_2000s.csv #{date} #{starting_offset_x} #{starting_offset_y} 0 23`
 end
