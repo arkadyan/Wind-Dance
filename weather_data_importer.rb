18bd34a3b7d746778455954ef23187fa0ea4b65f
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2011-02-22 23:18:26 -0500

    Updated the column types and numbers to match the current files we're working with.

--- weather_data_importer.rb
+++ weather_data_importer.rb
@@ -1,17 +1,33 @@
 module WeatherDataImporter
 	
-	RELATIVE_PRESSURE = 0
-	INDOOR_TEMPERATURE = 1
-	INDOOR_HUMIDITY = 2
-	OUTDOOR_TEMPERATURE = 3
-	OUTDOOR_HUMIDITY = 4
-	DEWPOINT = 5
-	WINDCHILL = 6
+	# RELATIVE_PRESSURE = 0
+	# INDOOR_TEMPERATURE = 1
+	# INDOOR_HUMIDITY = 2
+	# OUTDOOR_TEMPERATURE = 3
+	# OUTDOOR_HUMIDITY = 4
+	# DEWPOINT = 5
+	# WINDCHILL = 6
+	# WIND_SPEED = 7
+	# WIND_DIRECTION = 8
+	# RAIN_TOTAL = 9
+	# TIME = 10
+	# DATE = 11
+	
+	DATE = 0
+	TIME = 1
+	TEMPERATURE = 2
+	DEWPOINT = 3
+	PRESSURE = 4
+	WIND_DIRECTION = 5
+	WIND_DIRECTION_DEGREES = 6
 	WIND_SPEED = 7
-	WIND_DIRECTION = 8
-	RAIN_TOTAL = 9
-	TIME = 10
-	DATE = 11
+	WIND_SPEED_GUST = 8
+	HUMIDITY = 9
+	HOURLY_PRECIP = 10
+	CONDITIONS = 11
+	CLOUDS = 12
+	DAILY_RAIN = 13
+	SOFTWARE_TYPE = 14
 	
 	
 	def get_all_dates
@@ -33,7 +49,7 @@ module WeatherDataImporter
 		read_data(input_file).map do |line|
 			readings = line.chomp.split(',').map
 			current_hour = readings[TIME].split(':')[0]
-			if readings[DATE] == date && current_hour == hour
+			if readings[DATE] == date && (current_hour==hour || current_hour=='0'+hour)
 				hour_readings << {:time => readings[TIME], 
 					:wind_speed => readings[WIND_SPEED], :wind_direction => readings[WIND_DIRECTION]}
 			end
