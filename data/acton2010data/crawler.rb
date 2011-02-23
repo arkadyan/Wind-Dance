d25a3b5bdb9f569c3396c25a7144ce8da0c64b73
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2011-02-22 23:16:14 -0500

    Script to pull data from every day in a year from Weather Underground.

---
+++ data/acton2010data/crawler.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'rubygems'
+require 'rest_client'
+
+
+i = 0
+year = 2010
+(1..12).each do |month|
+	days_in_month = Date.civil(year, month, -1).day
+	(1..days_in_month).each do |day|
+		i += 1
+		
+		if i > 214
+			
+		url = "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KMAACTON4&month=#{month}&day=#{day}&year=#{year}&format=1"
+		output_file = "#{i}.csv"
+		puts url
+		csv = RestClient.get url
+		# Remove '<br>\n', the leading \n, the trailing comment, and the first line of column headers
+		csv.gsub!(/<br>\n/, '').gsub!(/^\n/, '').gsub!(/\n<!-- .+? -->\n$/, '').gsub!(/Time,TemperatureF,DewpointF,PressureIn,WindDirection,WindDirectionDegrees,WindSpeedMPH,WindSpeedGustMPH,Humidity,HourlyPrecipIn,Conditions,Clouds,dailyrainin,SoftwareType/, '')
+		File.open(output_file, 'w') {|f| f.write(csv) }
+	
+		sleep rand(60)  # seconds
+		
+		
+		end
+		
+	end
+end
