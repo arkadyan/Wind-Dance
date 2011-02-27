c567736deeac01ef700e20999a6c99b82bd326ea
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2011-02-26 19:43:03 -0500

    Updated the crawler to screen out files that just include the column headers and no further data.

--- data/acton2010data/crawler.rb
+++ data/acton2010data/crawler.rb
@@ -12,16 +12,21 @@ year = 2010
 	(1..days_in_month).each do |day|
 		i += 1
 		
-		if i > 214
+		if i > 217
+
 			
 		url = "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KMAACTON4&month=#{month}&day=#{day}&year=#{year}&format=1"
 		output_file = "#{i}.csv"
 		puts url
 		csv = RestClient.get url
-		# Remove '<br>\n', the leading \n, the trailing comment, and the first line of column headers
-		csv.gsub!(/<br>\n/, '').gsub!(/^\n/, '').gsub!(/\n<!-- .+? -->\n$/, '').gsub!(/Time,TemperatureF,DewpointF,PressureIn,WindDirection,WindDirectionDegrees,WindSpeedMPH,WindSpeedGustMPH,Humidity,HourlyPrecipIn,Conditions,Clouds,dailyrainin,SoftwareType/, '')
-		File.open(output_file, 'w') {|f| f.write(csv) }
-	
+		if csv.length > 193
+			# Remove '<br>\n', the leading \n, the trailing comment, and the first line of column headers
+			csv.gsub!(/<br>\n/, '').gsub!(/^\n/, '').gsub!(/\n<!-- .+? -->\n$/, '').gsub!(/Time,TemperatureF,DewpointF,PressureIn,WindDirection,WindDirectionDegrees,WindSpeedMPH,WindSpeedGustMPH,Humidity,HourlyPrecipIn,Conditions,Clouds,dailyrainin,SoftwareType/, '')
+			File.open(output_file, 'w') {|f| f.write(csv) }
+		else
+			puts "No data for #{year}-#{month}-#{day}, #{output_file}"
+		end
+			
 		sleep rand(60)  # seconds
 		
 		
