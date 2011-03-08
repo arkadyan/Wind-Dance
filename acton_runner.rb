854c0b967154fbd9a1008d52607c773160520cd7
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2011-03-07 19:56:24 -0500

    Updates made during the course of running through the entire year.

--- acton_runner.rb
+++ acton_runner.rb
@@ -1,8 +1,8 @@
-dates = '2010-01-11,2010-01-12,2010-01-13,2010-01-14,2010-01-15,2010-01-16,2010-01-17,2010-01-18,2010-01-19,2010-01-20'
+dates = '2010-02-05,2010-02-06,2010-02-07,2010-02-08,2010-02-09'
 puts "dates = #{dates}"
 
-starting_offset_x = 17000
-starting_offset_y = 17000
+starting_offset_x = 10000
+starting_offset_y = 8000
 
-size = 20000
-puts `rp5 run dance_diagram.rp5 DanceDiagram #{size} #{size} #{dates} data/acton2010data/acton_year_1-10.csv acton_year_1-10 #{starting_offset_x} #{starting_offset_y} 0 23`
+size = 16000
+puts `rp5 run dance_diagram.rp5 DanceDiagram #{size} #{size} #{dates} data/acton2010data/acton_year_36-40.csv acton_year_36-40 #{starting_offset_x} #{starting_offset_y} 0 23`
--- data/acton2010data/combine_days.rb
+++ data/acton2010data/combine_days.rb
@@ -1,14 +1,10 @@
 #!/usr/bin/env ruby
 
-# file_names = %x[ls -1 *.csv].split(/\n/).sort_by { |f| f.split('.').first.to_i }
 file_names = %x[ls -1 individual_days | grep '[0-9]' | sort -n].split(/\n/)
 
 combined_days = ''
 dates = []
-# file_names.each do |file_name|
-# 	IO.foreach(file_name) { |line| combined_days << line }
-# end
-(10..19).each do |i|
+(35..39).each do |i|
 	IO.foreach('individual_days/'+file_names[i]) do |line| 
 		combined_days << line
 		dates << line.split(' ').first
@@ -16,6 +12,12 @@ dates = []
 	combined_days << "\n"
 end
 
-# File.open('combined_year.csv', 'w') {|f| f.write(combined_days) }
-File.open('acton_year_11-20.csv', 'w') {|f| f.write(combined_days) }
+# Remove the final newline at the end of the file
+combined_days.chop!
+
+# Change ' ' after first column to ','
+combined_days.gsub!(' ', ',')
+
+File.open('acton_year_36-40.csv', 'w') {|f| f.write(combined_days) }
+
 puts dates.uniq.join(',')
