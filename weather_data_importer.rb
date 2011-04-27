ee8eb7dd0ac944b6f2716b56fcc2a8861cd23902
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2011-04-26 23:46:14 -0400

    Removed some old, unused methods and commented out constants from the weather_data_importer.

--- weather_data_importer.rb
+++ weather_data_importer.rb
@@ -1,18 +1,5 @@
 module WeatherDataImporter
 	
-	# RELATIVE_PRESSURE = 0
-	# INDOOR_TEMPERATURE = 1
-	# INDOOR_HUMIDITY = 2
-	# OUTDOOR_TEMPERATURE = 3
-	# OUTDOOR_HUMIDITY = 4
-	# DEWPOINT = 5
-	# WINDCHILL = 6
-	# WIND_SPEED = 7
-	# WIND_DIRECTION = 8
-	# RAIN_TOTAL = 9
-	# TIME = 10
-	# DATE = 11
-	
 	DATE = 0
 	TIME = 1
 	TEMPERATURE = 2
@@ -29,20 +16,6 @@ module WeatherDataImporter
 	DAILY_RAIN = 13
 	SOFTWARE_TYPE = 14
 	
-	
-	def get_all_dates
-	end
-	
-	def pull_times_for_date(input_file, date)
-		times = Array.new
-		read_data(input_file).map do |line|
-			values = line.chomp.split(',').map
-			if values[DATE] == date
-				times << values[TIME]
-			end
-		end
-		times
-	end
 	
 	def pull_data_for_date_hour(input_file, date, hour)
 		hour_readings = Array.new
